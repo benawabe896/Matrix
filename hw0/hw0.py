e700e1f917935cf4baacb88f12f46ca9deebbec7
Ben Powers <benawabe896@gmail.com>
2013-07-06 17:25:12 -0700

    Finished week 0

--- hw0/hw0.py
+++ hw0/hw0.py
@@ -21,39 +21,52 @@ def myFunctionComposition(f, g): return { x[0]:g[x[1]] for x in f.items()}
 ## Problem 4
 # Please only enter your numerical solution.
 
-complex_addition_a = ... 
-complex_addition_b = ...
-complex_addition_c = ...
-complex_addition_d = ...
+complex_addition_a = (5+3j)
+complex_addition_b = (1j)
+complex_addition_c = (-1+0.001j)
+complex_addition_d = (0.001+9j)
 
 
 
 ## Problem 5
-GF2_sum_1 = ...
-GF2_sum_2 = ...
-GF2_sum_3 = ...
+GF2_sum_1 = 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
 
 
 ## Problem 6
-def mySum(L): pass
-
-
+def mySum(L): 
+	current = 0
+	for x in L:
+		current += x
+	return current
 
 ## Problem 7
-def myProduct(L): pass
-
-
+def myProduct(L): 
+	current = 1
+	for x in L:
+		current *= x
+	return current
 
 ## Problem 8
-def myMin(L): pass
-
-
+def myMin(L): 
+	current = L[0]
+	for x in L:
+		if x < current:
+			current = x
+	return current
 
 ## Problem 9
-def myConcat(L): pass
-
-
+def myConcat(L): 
+	current = ''
+	for x in L:
+		current += x
+	return current
 
 ## Problem 10
-def myUnion(L): pass
+def myUnion(L): 
+	current = set()
+	for x in L:
+		current |= x
+	return current
 
