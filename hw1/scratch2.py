9e53609cfbd5abb549bdb2cf0bcfab616a06afdb
Sony <sony@Sonys-Mac-mini.local>
2013-07-10 09:14:43 -0700

    Adjusted triangular

--- hw1/scratch2.py
+++ hw1/scratch2.py
@@ -1,5 +1,6 @@
-def triangular_solve(rowlist, b):
-	x = zero_vec(rowlist[0].D)
-	for i in reversed(range(len(rowlist))):
-		x[i] = (b[i] - rowlist[i] * x)/rowlist[i][i]
+def triangular_solve(rowlist, label_list, b):
+	x = zero_vec(set(label_list))
+	for r in reversed(range(len(rowlist))):
+		c = label_list[r]
+		x[c] = (b[r] - x*rowlist[r])/rowlist[r][c]
 	return x
